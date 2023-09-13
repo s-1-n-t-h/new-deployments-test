7b7937192f267734faedd18ed048777f2aa32d9b
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-09-13 21:30:04 +0530

    changes for new model

--- app.py
+++ app.py
@@ -3,24 +3,30 @@ import torch
 import re
 import runpod
 
+
 def handler(event):
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('summarization', model="braindao/flan-t5-cnn",device=device)
+    model = pipeline(
+        "text-generation", model="TheBloke/Phind-CodeLlama-34B-v2-GPTQ", device=device
+    )
 
     # Parse out your arguments
-    prompt = event['input'].get('content',None)
+    prompt = event["input"].get("content", None)
 
-    CLEANER = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});|\\r|\\n|\s{2,}')
+    CLEANER = re.compile(
+        "<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});|\\r|\\n|\s{2,}"
+    )
 
     # cleaning text beforing feeding to model
     if prompt != None:
-        prompt = re.sub(CLEANER, '', prompt)
+        prompt = re.sub(CLEANER, "", prompt)
 
     if prompt == None:
-        return {'message': "No prompt provided"}
+        return {"message": "No prompt provided"}
 
     # Run the model
     result = model(prompt)
     return result
+
 
 runpod.serverless.start({"handler": handler})
